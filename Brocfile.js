5281e4632b19c8c26e87bcd4fa57b0660a4c01ca
Ricardo Mendes <rokusu@gmail.com>
2015-02-05 00:32:44 +0000

    Updates ember-cli and dependencies

--- Brocfile.js
+++ Brocfile.js
@@ -16,7 +16,12 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-app.import('bower_components/ember-localstorage-adapter/localstorage_adapter.js');
-
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', { destDir: 'assets' });
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', { destDir: 'fonts' });
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', { destDir: 'fonts' });
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', { destDir: 'fonts' });
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', { destDir: 'fonts' });
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
 module.exports = app.toTree();
